372c0b706d6b3a30f08f94aaaeea3d0b2863e174
Mads <mk@magnutron.dk>
2024-03-11 15:15:44 +0100

    Yes

---
+++ src/main/java/dat3/rename_me/entity/Activity.java
@@ -0,0 +1,40 @@
+package dat3.rename_me.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String name;
+
+    private double price;
+
+    String base64image;
+
+    @Column(columnDefinition="TEXT")
+    String description;
+
+    byte capacity;
+
+    int ageLimit;
+
+    boolean isActive;
+
+    int cancelLimit;
+
+    int timeSpan;
+
+}
---
+++ src/main/java/dat3/rename_me/entity/Booking.java
@@ -0,0 +1,50 @@
+package dat3.rename_me.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable =true)
+    String companyName;
+
+    String customerFirstName;
+
+    String customerLastName;
+
+    String streetName;
+
+    String streetNumber;
+
+    int zipCode;
+
+    String city;
+
+    int phoneNumber;
+
+    int bookingNumber;
+
+    String activity;
+
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @UpdateTimestamp
+    private LocalDateTime edited;
+}
