955788ad0345b1a6303775dfba2c87123059f1b1
AdamWarfa <Adamwarfadk@gmail.com>
2024-03-22 20:40:07 +0100

    AD-37 SQL DONE

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,7 +16,7 @@ INSERT INTO adventurexp.activity_events (id, activity_id, start_time, available_
 INSERT INTO adventurexp.activity_events (id, activity_id, start_time, available_spots, capacity) VALUES (UNHEX(REPLACE('b1b2c3d4-1234-5678-90ab-cdef12345677', '-', '')), UNHEX(REPLACE('a1b2c3d4-1234-5678-90ab-cdef12345678', '-', '')), '2024-03-22 11:30:00', 10, 10);
 
 -- Insert customers
-INSERT INTO adventurexp.customers (id, company_name, cvr, is_company, first_name, last_name, email, street_name, street_number, zip_code, city, phone_number, username) VALUES (UNHEX(REPLACE('c1b2c3d4-1234-5678-90ab-cdef12345675', '-', '')), 'MagnuTron ApS', 37930385, 1, 'Mads', 'Kristensen', 'mk@magnutron.dk', 'Tagensvej', '169 TH', 2400, 'KBH NV', 21278060, 'magnutron');
+INSERT INTO adventurexp.customers (id, company_name, cvr, is_company, first_name, last_name, email, street_name, street_number, zip_code, city, phone_number, username) VALUES (UNHEX(REPLACE('c1b2c3d4-1234-5678-90ab-cdef12345675', '-', '')), 'MagnuTron ApS', 37930385, true, 'Mads', 'Kristensen', 'mk@magnutron.dk', 'Tagensvej', '169 TH', 2400, 'KBH NV', 21278060, 'magnutron');
 
 -- Insert bookings
 INSERT INTO adventurexp.bookings (id, customer_id, activity_event_id, booking_number, participants) VALUES (UNHEX(REPLACE('d1b2c3d4-1234-5678-90ab-cdef12345675', '-', '')), UNHEX(REPLACE('c1b2c3d4-1234-5678-90ab-cdef12345675', '-', '')), UNHEX(REPLACE('b1b2c3d4-1234-5678-90ab-cdef12345675', '-', '')), 1, 4);
